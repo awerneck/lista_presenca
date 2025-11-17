9798666b25eb594032b5f0396a65cfcd66c490a7
Allan Werneck <edielwerneck@gmail.com>
2025-11-17 13:59:41 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,21 +1,30 @@
 function toggleTheme() {
-    const theme = document.getElementById("theme");
-    theme.href = theme.href.includes("dark") ?
-        "/static/bootstrap.css" : "/static/bootstrap-dark.css";
+  const theme = document.getElementById("theme");
+  theme.href = theme.href.includes("dark")
+    ? "/static/bootstrap.css"
+    : "/static/bootstrap-dark.css";
 }
 
-document.getElementById('busca')?.addEventListener('input', function () {
-    const filtro = this.value.toLowerCase();
-    document.querySelectorAll("#tabela tbody tr").forEach(tr => {
-        tr.style.display = tr.textContent.toLowerCase().includes(filtro) ? "" : "none";
-    });
-});
+// Atualização do QR na Home
+if (document.getElementById("qrcode")) {
+    function refreshQRHome() {
+        fetch("/api/token")
+        .then(r => r.json())
+        .then(data =>
+        {
+            document.getElementById("qrcode").innerHTML = "";
+            new QRCode(document.getElementById("qrcode"),
+                window.location.origin + "/presenca?token=" + data.token
+            );
+        });
+    }
+    setInterval(refreshQRHome, 60000);
+}
 
-fetch("/api/graficos")
-.then(r => r.json())
-.then(data => {
-    new Chart(document.getElementById("grafSetor"), {
-        type: "pie",
-        data: { labels: Object.keys(data.setores), datasets: [{ data: Object.values(data.setores) }] }
-    });
+// Busca admin
+document.getElementById('busca')?.addEventListener('input', function () {
+  const filtro = this.value.toLowerCase();
+  document.querySelectorAll("#tabela tbody tr").forEach(tr => {
+      tr.style.display = tr.textContent.toLowerCase().includes(filtro) ? "" : "none";
+  });
 });
